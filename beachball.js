8664f61087e6a6fa7316803fb3200a02d5b3d8ed
Graeme Clark <grae0026@stud.kea.dk>
2020-03-18 20:35:31 +0100

    Add functions for animation

--- beachball.js
+++ beachball.js
@@ -1,3 +1,29 @@
 "use strict";
 
-gsap.to(".ball", { duration: 4, x: 300, rotate: 200 });
+// gsap.from(".ball", { duration: 2, x: 300, rotate: 200, ease: "bounce" });
+
+// var myObject = { rotation: 0 };
+// gsap.to(myObject, {
+// 	duration: 2,
+// 	rotate: 300,
+// 	onComplete: function() {
+// 		console.log("hello" + myObject.rotation);
+// 	}
+// });
+
+// gsap.from(".ball", { duration: 2, opacity: 0, y: 150, stagger: 0.25 });
+
+gsap.to(".ball", { duration: 2, x: 400, rotate: 200, ease: "bounce", delay: 3, onStart: beginAnim, onUpdate: inProgress, onComplete: animCompleted });
+
+function beginAnim() {
+	console.log("animation started");
+}
+
+function inProgress() {
+	console.log("in progress");
+}
+
+function animCompleted() {
+	console.log("done!");
+	gsap.to(".ball", { duration: 4, scale: 0.1 });
+}
